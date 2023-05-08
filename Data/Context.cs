aa02fc9ff7a9a06ac266eb86950d4753f705c97b
Alvin strandberg <Str4ndb3rg@live.se>
2023-05-08 15:02:13 +0200

    First commit, created models.

---
+++ Data/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeReportClassLibrary.Models;
+namespace TimeReportAPI.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TimeReport> TimeReports { get; set; }
+        public DbSet<ProjectEmployee> ProjectEmployees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}

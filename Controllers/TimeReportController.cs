780d6b5cee1a5f96deeb0ea3c35a7e3a3d1cbab9
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-05-11 15:13:01 +0200

    Basic CRUD repository, updated TimeReportController

--- Controllers/TimeReportController.cs
+++ Controllers/TimeReportController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TimeReportAPI.Repositories.Interfaces;
+using TimeReportClassLibrary.Models;
 
 namespace TimeReportAPI.Controllers
 {
@@ -8,18 +9,80 @@ namespace TimeReportAPI.Controllers
     public class TimeReportController : ControllerBase
     {
         private readonly ITimeReportRepository _timeReportRepository;
+        private readonly IRepository<TimeReport> _repository;
 
-        public TimeReportController(ITimeReportRepository timeReportRepository)
+        public TimeReportController(ITimeReportRepository timeReportRepository, IRepository<TimeReport> repository)
         {
             _timeReportRepository = timeReportRepository;
+            _repository = repository;
         }
 
-        [HttpGet("GetHoursByWeekYearEmployee")]
-        public async Task<ActionResult<double>> GetHoursByWeekYearEmployee([FromQuery] int year, int week, int employeeId)
+        [HttpGet("GetWeeklyTimeReport/{year:int}/{week:int}/{employeeId:int}")]
+        public async Task<ActionResult<double>> GetWeeklyTimeReport(int year, int week, int employeeId)
         {
             try
             {
                 return await _timeReportRepository.GetHoursByWeek(year, week, employeeId);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+
+        [HttpPost("CreateTimeReport/")]
+        public async Task<ActionResult<TimeReport>> CreateTimeReport([FromBody] TimeReport timeReport)
+        {
+            try
+            {
+                return await _repository.Add(timeReport);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+
+        [HttpGet("ReadTimeReport/{id:int}")]
+        public async Task<ActionResult<TimeReport>> ReadTimeReport(int id)
+        {
+            try
+            {
+                return await _repository.Get(id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+
+        [HttpPut("UpdateTimeReport/")]
+        public async Task<ActionResult<TimeReport>> UpdateTimeReport([FromBody] TimeReport timeReport)
+        {
+            try
+            {
+                return await _repository.Update(timeReport);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+
+        [HttpDelete("DeleteTimeReport/{id:int}")]
+        public async Task<ActionResult<TimeReport>> DeleteTimeReport(int id)
+        {
+            try
+            {
+                return await _repository.Delete(id);
             }
             catch (Exception e)
             {
---
+++ Repositories/Repository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeReportAPI.Data;
+using TimeReportAPI.Repositories.Interfaces;
+
+namespace TimeReportAPI.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly Context _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(Context context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T> Add(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Delete(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity == null)
+            {
+                return null;
+            }
+
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Get(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}

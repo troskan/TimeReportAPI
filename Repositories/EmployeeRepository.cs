5523af288d17300209b1c1c58f3f4b18f1dbd2ed
Alvin strandberg <Str4ndb3rg@live.se>
2023-05-15 11:02:25 +0200

    removed crud and inherited it from Repository instead.

--- Repositories/EmployeeRepository.cs
+++ Repositories/EmployeeRepository.cs
@@ -7,10 +7,11 @@ using TimeReportClassLibrary.Models;
 
 namespace TimeReportAPI.Repositories
 {
-    public class EmployeeRepository : IRepository<Employee>, IEmployeeRepository<Employee>
+    public class EmployeeRepository : Repository<Employee>, IEmployeeRepository<Employee>
     {
         private readonly Context _db;
-        public EmployeeRepository(Context db) 
+
+        public EmployeeRepository(Context db) : base(db)  // calling base constructor here
         {
             _db = db;
         }
@@ -31,38 +32,8 @@ namespace TimeReportAPI.Repositories
                                       };
 
             return await detailedTimeReports.ToListAsync();
-        
-
-
-
-        //List<EmployeeTimeReportDTO> listOfReports = new List<EmployeeTimeReportDTO>();
-
-        //foreach (var i in detailedTimeReports)
-        //{
-        //    var emp = new EmployeeTimeReportDTO();
-        //    emp.EmployeeID = i.EmployeeID;
-        //    emp.FirstName = i.FirstName;
-        //    emp.LastName = i.LastName;
-        //    emp.ProjectID = i.ProjectID;
-        //    emp.ProjectName = i.ProjectName;
-        //    emp.StartTime = i.StartTime;
-        //    emp.EndTime = i.EndTime;
-
-        //    listOfReports.Add(emp);
-        //}
-        //return Task.FromResult(listOfReports);
-
-    }
-
-
-
-
-
-
-
-
-
-    public async Task<IEnumerable<Employee>> GetEmployeesByProject(int id)
+        }
+        public async Task<IEnumerable<Employee>> GetEmployeesByProject(int id)
         {
             var projects = from emp in _db.Employees
                            join pe in _db.ProjectEmployees on emp.EmployeeID equals pe.EmployeeID
@@ -72,114 +43,6 @@ namespace TimeReportAPI.Repositories
                            select emp;
 
             return await projects.ToListAsync();
-        }
-        public async Task<Employee> Add(Employee entity)
-        {
-            if (entity.FirstName == null || entity.LastName == null) { return null; }
-            if (entity.FirstName.Length < 2 || entity.LastName.Length < 2) { return null; }
-            
-            try
-            {
-                await _db.AddAsync(entity);
-                await _db.SaveChangesAsync();
-                return entity;
-
-            }
-            catch (Exception e)
-            {
-
-                Console.WriteLine(e.Message);
-                return null;
-            }
-        }
-
-        public async Task<Employee> Delete(int id)
-        {
-
-            try
-            {
-                var employeeToDelete = await _db.Employees.FirstOrDefaultAsync(e => e.EmployeeID == id);
-
-                if (employeeToDelete == null)
-                {
-                    return null;
-                }
-
-                _db.Remove(employeeToDelete);
-                await _db.SaveChangesAsync();
-
-                return employeeToDelete;
-
-            }
-            catch (Exception e)
-            {
-
-                Console.WriteLine(e.Message);
-                return null;
-            }
-        }
-
-        public async Task<Employee> Get(int id)
-        {
-
-            try
-            {
-                var employeeToGet = await _db.Employees.FirstOrDefaultAsync(e => e.EmployeeID == id);
-
-                if (employeeToGet == null)
-                {
-                    return null;
-                }
-
-                return employeeToGet;
-
-            }
-            catch (Exception e)
-            {
-
-                Console.WriteLine(e.Message);
-                return null;
-            }
-        }
-
-        public async Task<IEnumerable<Employee>> GetAll()
-        {
-            try
-            {
-                return await _db.Employees.ToListAsync();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return null;
-            }
-        }
-
-
-
-        public async Task<Employee> Update(Employee entity)
-        {
-            //if (entity.FirstName == null || entity.LastName == null) { return null; }
-            //if (entity.FirstName.Length < 2 || entity.LastName.Length < 2) { return null; }
-            if (entity.EmployeeID == null || entity.EmployeeID == 0) { return null; }
-
-            try
-            {
-                var employeeToUpdate = await _db.Employees.FirstOrDefaultAsync(e=>e.EmployeeID == entity.EmployeeID);
-                employeeToUpdate.FirstName = entity.FirstName;
-                employeeToUpdate.LastName = entity.LastName;
-
-                await _db.SaveChangesAsync();
-
-                return entity;
-
-            }
-            catch (Exception e)
-            {
-
-                Console.WriteLine(e.Message);
-                return null;
-            }
         }
     }
 }
